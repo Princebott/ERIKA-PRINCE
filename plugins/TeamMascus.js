0a05a45916a1d505faf028b7c89bee1b457a92b1
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-14 17:47:10 +0100

    Update TeamMascus.js

--- plugins/TeamMascus.js
+++ plugins/TeamMascus.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamMasc)$/i;
 export default handler;
 
 global.TeamMasc = `
-🩵 𝗚𝗼𝘁𝘁𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🤍
+𝗕𝗼𝘁 𝗞𝗰𝗵𝘂𝗱𝗮𝘀 🦌
 
 *🦍 Team Time Long*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
