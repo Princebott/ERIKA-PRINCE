a7087fbf306c09f06bb27ca225bb72d7e1098e11
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-14 17:46:52 +0100

    𝗕𝗼𝘁 𝗞𝗰𝗵𝘂𝗱𝗮𝘀 🦌

--- plugins/TeamFemes.js
+++ plugins/TeamFemes.js
@@ -5,7 +5,7 @@ handler.command = /^(TeamFem)$/i;
 export default handler;
 
 global.TeamFem = `
-🩵 𝗚𝗼𝘁𝘁𝗶 𝗥𝗲𝗰𝗹𝘂𝘁𝗮𝗺𝗶𝗲𝗻𝘁𝗼𝘀 🤍
+𝗕𝗼𝘁 𝗞𝗰𝗵𝘂𝗱𝗮𝘀 🦌
 
 *Time Long Fem 💋*
 https://chat.whatsapp.com/LILrwBpgQYQ5leRfcBNwyS
