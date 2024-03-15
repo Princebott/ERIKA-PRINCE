807aa5bb9192e2c24af7fb40fccc1f3071b8daf0
BotsDeClientes <161894321+BotsDeClientes@users.noreply.github.com>
2024-03-15 13:35:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Quesito\nBot', {
+say('Prince\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
